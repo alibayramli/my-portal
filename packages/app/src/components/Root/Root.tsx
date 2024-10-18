192d5fe92be141773ab01cd7168ea37f4291b5be
Ali Bayramli <alibayramli@tutanota.com>
2024-10-18 14:49:48 +0400

    Register catalog in sidebar

--- packages/app/src/components/Root/Root.tsx
+++ packages/app/src/components/Root/Root.tsx
@@ -4,6 +4,7 @@ import HomeIcon from '@material-ui/icons/Home';
 import ExtensionIcon from '@material-ui/icons/Extension';
 import LibraryBooks from '@material-ui/icons/LibraryBooks';
 import CreateComponentIcon from '@material-ui/icons/AddCircleOutline';
+import CreateIcon from '@material-ui/icons/Create';
 import LogoFull from './LogoFull';
 import LogoIcon from './LogoIcon';
 import {
@@ -74,7 +75,12 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
         />
         <SidebarItem icon={ExtensionIcon} to="api-docs" text="APIs" />
         <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
-        <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
+        <SidebarItem icon={CreateIcon} to="create" text="Create..." />
+        <SidebarItem
+          icon={CreateComponentIcon}
+          to="catalog-import"
+          text="Register"
+        />
         {/* End global nav */}
         <SidebarDivider />
         <SidebarScrollWrapper>
